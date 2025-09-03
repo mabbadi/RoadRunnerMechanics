9c0418f26938855836bbecca4ee755edf57f26e0
mabbadi <mabbadi@outlook.com>
2025-09-03 21:56:49 +0200

    test

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
@@ -16,7 +15,8 @@ export default defineConfig(({ mode }) => ({
   ].filter(Boolean),
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./RoadRunnerMechanics"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: './src' // Since this will be the base directory
 }));
